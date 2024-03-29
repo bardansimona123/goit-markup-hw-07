e4eac81bfa3083a146b50dd2f421492aef7e8ee7
bardansimona123 <bardan.simona2006@gmail.com>
2024-03-29 21:20:12 +0000

    tema 7 tableta

---
+++ js/mobile-menu.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menu = document.getElementById("menuOptions");
+    menu.classList.toggle("show");
+   }
+  
+   function hideMenu() {
+    var menuOptions = document.getElementById("menuOptions");
+    menuOptions.classList.remove("show");
+   }
--- js/modal.js
+++ js/modal.js
@@ -13,12 +13,4 @@
   })();
 
 
- function toggleMenu() {
-  var menu = document.getElementById("menuOptions");
-  menu.classList.toggle("show");
- }
-
- function hideMenu() {
-  var menuOptions = document.getElementById("menuOptions");
-  menuOptions.classList.remove("show");
- }
+ 
